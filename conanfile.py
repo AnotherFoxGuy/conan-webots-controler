d1dbd39059e03386ebe83553073af1cb0ff7786d
Edgar <Edgar@AnotherFoxGuy.com>
2020-04-23 10:14:18 +0200

    :tada: Initial commit

---
+++ conanfile.py
@@ -0,0 +1,26 @@
+from conans import ConanFile, CMake, tools
+from conans.tools import os_info, SystemPackageTool
+
+
+class WebotscontrollerConan(ConanFile):
+    name = "webots-controller"
+    version = "R2020a-rev1"
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Webotscontroller here>"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+    exports_sources = "src/*"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="src")
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)

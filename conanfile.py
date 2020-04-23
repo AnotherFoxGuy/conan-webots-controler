c2c3e60db775eec585e13bbe758a45dfc78c124c
Edgar <Edgar@AnotherFoxGuy.com>
2020-04-23 12:59:28 +0200

    :bug: Fixed build on windows

--- conanfile.py
+++ conanfile.py
@@ -11,16 +11,21 @@ class WebotscontrollerConan(ConanFile):
     description = "<Description of Webotscontroller here>"
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    exports_sources = "src/*"
+    exports_sources = ["src/*", "prebuild/*"]
 
     def build(self):
         cmake = CMake(self)
+        if tools.os_info.is_windows and self.settings.compiler == "Visual Studio":
+            cmake.definitions['BUILD_C'] = 'OFF'
         cmake.configure(source_folder="src")
         cmake.build()
 
     def package(self):
         cmake = CMake(self)
         cmake.install()
+        if tools.os_info.is_windows and self.settings.compiler == "Visual Studio":
+            self.copy("*.lib", dst="lib", keep_path=False)
+            self.copy("*.dll", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
